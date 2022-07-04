0197b01ccc492f604143eb3368ec54230f73e4d3
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-07-04 17:10:30 -0500

    coding challange 3

--- js/js-course.js
+++ js/js-course.js
@@ -228,3 +228,37 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
   console.log('Someone else should drive...');
 }
+
+// Coding Challenge #3
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each
+// other 3 times. The winner with the highest average score wins a trophy!
+
+// Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+
+// 1. Calculate the average score for each team, using the test data below
+
+function average (a, b, c){
+  return(a +b + c) / 3
+}
+
+console.log("Dolphins' score average: " + average(96, 108, 89).toFixed(0));
+const dolphins = 98;
+console.log("Koala's score average: " + average(109, 95, 123).toFixed(0));
+const koalas = 109;
+
+// 2. Compare the team's average scores to determine the winner of the competition,
+// and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score)
+
+function theWinnerIs (dolphins, koalas) {
+  if (dolphins > koalas) {
+    console.log("Dolphins' win!")
+  } else {
+    (koalas > dolphins)
+    console.log("Koala's win!")
+
+  }
+}
+
+theWinnerIs();
