cdbf30fe555816f0d7b11a3be3f00ac3d711693d
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-07-04 18:25:59 -0500

    coding challange 4

--- js/js-course.js
+++ js/js-course.js
@@ -330,3 +330,22 @@ console.log(`I like to drink ${newAge >= 18 ? 'wine 🍷' : 'water 💧'}`);
 // restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
 // 300. If the value is different, the tip is 20%.
 
+// Coding Challenge #4
+
+// § Data 1: Test for bill values 275, 40 and 430
+
+// Steven wants to build a very simple tip calculator for whenever he goes eating in a
+// restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+// 300. If the value is different, the tip is 20%.
+
+
+// const bill = 275;
+// const bill = 40;
+const bill = 430;
+
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+// const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+
