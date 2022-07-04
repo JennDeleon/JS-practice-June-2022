beff74434b898e1567420fc39dbf5668482e7651
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-07-04 17:33:15 -0500

    ternary

--- js/js-course.js
+++ js/js-course.js
@@ -312,4 +312,12 @@ const age = 23;
 const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
 console.log(drink);
 
+let drink2;
+if (age >= 18) {
+  drink2 = 'wine 🍷';
+} else {
+  drink2 = 'water 💧';
+}
+console.log(drink2);
 
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
