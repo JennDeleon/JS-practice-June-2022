38c54acc515e0e938c5c0d568cb4664b6b1db484
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-07-04 17:22:58 -0500

    m

--- js/js-course.js
+++ js/js-course.js
@@ -291,3 +291,16 @@ if (day === 'monday') {
 } else {
   console.log('Not a valid day!');
 }
+
+
+// Statements and Expressions
+
+
+// if (23 > 10) {
+//   const str = '23 is bigger';
+// }
+//
+// const me = 'Jonas';
+// console.log(`I'm ${2037 - 1991} years old ${me}`);
+
+
