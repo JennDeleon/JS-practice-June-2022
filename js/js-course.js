0424fc194c0e112d2f51e8b5961649c22d300cbe
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-07-13 18:50:12 -0500

    m

--- js/js-course.js
+++ js/js-course.js
@@ -501,3 +501,16 @@ const calcAge2 = function (birthYeah) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+
+// Arrow functions
+
+const calcAge3 = birthYeah => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYeah, firstName) => {
+  const age = 2037 - birthYeah;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+}
