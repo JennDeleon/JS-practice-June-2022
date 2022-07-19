05320ae11bdde1549b70dd6803f78ee393b16e1d
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-07-18 19:56:24 -0500

    m

--- js/js-course.js
+++ js/js-course.js
@@ -570,3 +570,6 @@ A team ONLY wins if it has at least DOUBLE the average score of the other team. 
 // TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
 // TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
 
+// HINT: To calculate average of 3 values, add them all together and divide by 3
+// HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores
+
