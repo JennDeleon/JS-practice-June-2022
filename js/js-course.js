e30a53d3fc272186370d1d809423ef4dd0527033
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-07-20 21:32:25 -0500

    m

--- js/js-course.js
+++ js/js-course.js
@@ -611,3 +611,8 @@ checkWinner(scoreDolphins, scoreKoalas);
 const friend1 = 'Michael';
 const friend2 = 'Steven';
 const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
